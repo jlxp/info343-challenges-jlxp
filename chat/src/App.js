4df62da57da0092e931e9cd17a917f2980f4a005
Joy Phillips <jlxp@uw.edu>
2018-02-20 15:57:58 -0800

    firebase added

--- chat/src/App.js
+++ chat/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
 class App extends Component {
   render() {
     return (
--- chat/src/index.js
+++ chat/src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import firebase from "firebase/app";
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAq1jj1yFhQD6g6yl9XZtJhSKM6f1JxINM",
+    authDomain: "architecting-apps-de4d0.firebaseapp.com",
+    databaseURL: "https://architecting-apps-de4d0.firebaseio.com",
+    projectId: "architecting-apps-de4d0",
+    storageBucket: "architecting-apps-de4d0.appspot.com",
+    messagingSenderId: "872139235286"
+};
+firebase.initializeApp(config);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
