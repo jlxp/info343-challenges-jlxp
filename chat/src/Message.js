b900f3a2bbd0776542834f7125ffb504b8d45249
Joy Phillips <jlxp@uw.edu>
2018-02-25 22:54:44 -0800

    timestamp works!

--- chat/src/Message.js
+++ chat/src/Message.js
@@ -3,11 +3,12 @@ import React from "react";
 export default class Message extends React.Component {
     render() {
         let message = this.props.messageSnap.val();
-        let date = new Date(message.createdAt / 1000);
+        let date = new Date(message.createdAt);
         console.log(date);
         return (
             <div>
                 <h6 className="font-weight-bold"> {message.author.name} </h6>
+                <p>{date.toString()}</p>
                 <p> {message.body} </p>
             </div>
         )
